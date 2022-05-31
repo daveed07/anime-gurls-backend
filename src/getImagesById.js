15200c2d00d577963cb2c43b421df52579379d8c
David Zuñiga <davidz0703@gmail.com>
2022-05-31 09:53:24 -0500

    Fixing request bug

--- src/getImagesById.js
+++ src/getImagesById.js
@@ -3,7 +3,7 @@ const pool = require('./pool.config').pool;
 exports.getImagesById = async (req, res) => {
   try {
     const client = await pool.connect();
-    const result = await client.query("SELECT * FROM images WHERE id = $1", [req.params.id]);
+    const result = await client.query("SELECT * FROM girl WHERE id = $1", [req.params.id]);
     res.status(200).json(result.rows[0]);
     client.release();
   } catch (err) {
