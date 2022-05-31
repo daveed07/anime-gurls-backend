ccaa3d22fa894fa7508a8931eb0e676eb039f685
David Zuñiga <davidz0703@gmail.com>
2022-05-31 09:43:23 -0500

    Fixing request bug

--- src/getImages.js
+++ src/getImages.js
@@ -4,8 +4,7 @@ exports.getImages = async (req, res) => {
   try {
     const client = await pool.connect();
     const result = await client.query("SELECT * FROM girl");
-    const results = result ? result.rows : null;
-    res.status(200).json({ results });
+    res.status(200).json(result);
     client.release();
   } catch (err) {
     console.log(err);
